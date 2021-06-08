cb12f6297b1ade92c2971f97bcf3bfb9e8351299
AOG <grantaom@gmail.com>
2021-06-08 13:19:50 -0400

    Added a GUI feature

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import csv
 import datetime as dt
 import time
+import PySimpleGUI as sg
+import tkinter as tk
+import os
 
 # Role: Open and read file
 def open_file(csvfile):
@@ -46,30 +49,47 @@ def strip_columns(file, newfilename):
  
                 writer.writerow((r[6], r[9], r[10], r[11], r[12], r[13], r[14], r[17]))
 
-def main(file):
+def main():
     
-    csvfile = open_file(file)
-    header = csvfile[0]
-    body = csvfile[1:]
+    # GUI
+    #################################
+    sg.ChangeLookAndFeel('BrownBlue')
+    form = sg.FlexForm('Sysco csv to xlsx conversion tool')
+    layout = [
+          [sg.Text('Your Files', size=(15, 1), auto_size_text=False, justification='right'),
+          sg.InputText('Click "Browse" to input file'), sg.FileBrowse()],
+          [sg.Submit(), sg.Cancel()]
+        ]
+    button, values = form.Layout(layout).Read()
+    file = values[0]
+    ################################
 
-    # creates the new file title
-    new_filename = 'Sysco' + get_dates(body) + customer_name_filename(body) +'.xlsx'
+    if ".csv" not in file:
+        return  sg.Popup("Please input a .csv file")
+    else:
+        csvfile = open_file(file)
+            
+        header = csvfile[0]
+        body = csvfile[1:]
 
-    split_idx = header.index("Split")
-    qty_idx = header.index("Qty")
-    pack_idx = header.index("Pack")
+        # creates the new file title
+        new_filename = 'Sysco' + get_dates(body) + customer_name_filename(body) +'.xlsx'
 
-    # This makes the Pack values, Qty values on Split
-    for row in body:
-        split = row[split_idx]
-        qty = row[qty_idx]
-        if split != '':
-            row[pack_idx] = qty
-    
-    # this makes a new file from the results of the new_filename var
-    strip_columns(file, new_filename) 
-    
+        split_idx = header.index("Split")
+        qty_idx = header.index("Qty")
+        pack_idx = header.index("Pack")
 
+        # This makes the Pack values, Qty values on Split
+        for row in body:
+            split = row[split_idx]
+            qty = row[qty_idx]
+            if split != '':
+                row[pack_idx] = qty
+        
+        # this makes a new file from the results of the new_filename var
+        strip_columns(file, new_filename) 
+
+        sg.Popup("Conversion Successul!\n\nYour new file\n\n'{}'\n\nwill be in the\n\n'{}' folder.".format(new_filename, os.getcwd()))
 
 if __name__ == "__main__":
-    main(input())
+    main()
